67fcddf367cc08581b5d32e45657069978c46108
Jek John Tan <jetan@u90z03s07.42kl.edu.my>
2024-09-27 17:26:17 +0800

    upload minishell.h

---
+++ minishell.h
@@ -0,0 +1,15 @@
+#ifndef MINI_SHELL_H
+# define MINI_SHELL_H
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include "../libft/libft.h"
+
+#include "builtins.h"
+
+
+
+# endif
