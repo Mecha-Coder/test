b180de864989d565ecf298c3d0657c7d24fc1503
Jek John Tan <jetan@u90z03s07.42kl.edu.my>
2024-09-29 19:20:27 +0800

    update exit.c(haven't finish)

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 16:29:03 by jetan             #+#    #+#             */
-/*   Updated: 2024/09/28 20:34:23 by jetan            ###   ########.fr       */
+/*   Updated: 2024/09/29 19:14:42 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,27 @@ void	builtins_exit(char **av)
 	
 	if (av[1])
 	{
-		if (is_numeric())
+		if (is_numeric(av[1]))
+		{
+			exit_status = ft_atoi(av[1]);
+			printf("exit\n");
+			exit(exit_status);
+		}
+		else
+		{
+			printf("exit: %s: numeric argument required\n", av[1]);
+			exit(1);
+		}
 	}
+	else
+	{
+		printf("exit\n");
+		exit(0);
+	}
+}
+
+int main(int ac, char **av)
+{
+	(void)ac;
+	builtins_exit(av);
 }
