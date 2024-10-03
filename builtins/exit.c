13eb670a5938c205bbc985d6aa55f052621d372f
Jek John Tan <jetan@u90z02s01.42kl.edu.my>
2024-10-03 16:55:13 +0800

    exit.c update

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 16:29:03 by jetan             #+#    #+#             */
-/*   Updated: 2024/09/30 16:40:09 by jetan            ###   ########.fr       */
+/*   Updated: 2024/10/03 16:54:32 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,33 +27,60 @@ int	is_numeric(char *str)
 	return (1);
 }
 
-void	builtins_exit(char **av)
+// void	builtin_exit(char **av)
+// {
+// 	int	exit_status;
+	
+// 	if (av[1])
+// 	{
+// 		if (is_numeric(av[1]))
+// 		{
+// 			exit_status = ft_atoi(av[1]);
+// 			printf("exit\n");
+// 			exit(exit_status);
+// 		}
+// 		else
+// 		{
+// 			printf("exit: %s: numeric argument required\n", av[1]);
+// 			exit(1);
+// 		}
+// 	}
+// 	else
+// 	{
+// 		printf("exit\n");
+// 		exit(0);
+// 	}
+// }
+
+void	builtin_exit(char **av)
 {
 	int	exit_status;
 	
-	if (av[1])
+	exit_status = 0;
+	if (av[1] == NULL)
 	{
-		if (is_numeric(av[1]))
-		{
-			exit_status = ft_atoi(av[1]);
-			printf("exit\n");
-			exit(exit_status);
-		}
-		else
-		{
-			printf("exit: %s: numeric argument required\n", av[1]);
-			exit(1);
-		}
+		ft_putstr_fd("exit\n", 2);
+		exit(exit_status);
 	}
-	else
+	if (!is_numeric(av[1]))
 	{
-		printf("exit\n");
-		exit(0);
+		// error_message();
+		write(2, "exit: a: numeric argument required\n", 32);
+		exit(2);
 	}
+	if (av[1] && av[2])
+	{
+		ft_putstr_fd("exit: too many arguments\n", 2);
+		return;
+	}
+	exit_status = ft_atoi(av[1]);
+	ft_putstr_fd("exit\n", 2);
+	exit (exit_status);
+	
 }
-//haven't handle the too many argument ex: exit 4 2
+//haven't handle error message
 int main(int ac, char **av)
 {
 	(void)ac;
-	builtins_exit(av);
+	builtin_exit(av);
 }
