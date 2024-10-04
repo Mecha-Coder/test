b6f998a5545209adc2427d9a3902b7b9aca2da72
Jek John Tan <jetan@u90z04s02.42kl.edu.my>
2024-10-04 16:26:42 +0800

    completed error message

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 16:29:03 by jetan             #+#    #+#             */
-/*   Updated: 2024/10/03 16:54:32 by jetan            ###   ########.fr       */
+/*   Updated: 2024/10/04 16:25:00 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,31 +27,6 @@ int	is_numeric(char *str)
 	return (1);
 }
 
-// void	builtin_exit(char **av)
-// {
-// 	int	exit_status;
-	
-// 	if (av[1])
-// 	{
-// 		if (is_numeric(av[1]))
-// 		{
-// 			exit_status = ft_atoi(av[1]);
-// 			printf("exit\n");
-// 			exit(exit_status);
-// 		}
-// 		else
-// 		{
-// 			printf("exit: %s: numeric argument required\n", av[1]);
-// 			exit(1);
-// 		}
-// 	}
-// 	else
-// 	{
-// 		printf("exit\n");
-// 		exit(0);
-// 	}
-// }
-
 void	builtin_exit(char **av)
 {
 	int	exit_status;
@@ -64,8 +39,8 @@ void	builtin_exit(char **av)
 	}
 	if (!is_numeric(av[1]))
 	{
-		// error_message();
-		write(2, "exit: a: numeric argument required\n", 32);
+		ft_putstr_fd("exit\n", 2);
+		error_message("exit", av[1], "numeric argument required");
 		exit(2);
 	}
 	if (av[1] && av[2])
@@ -78,9 +53,8 @@ void	builtin_exit(char **av)
 	exit (exit_status);
 	
 }
-//haven't handle error message
-int main(int ac, char **av)
-{
-	(void)ac;
-	builtin_exit(av);
-}
+// int main(int ac, char **av)
+// {
+// 	(void)ac;
+// 	builtin_exit(av);
+// }
