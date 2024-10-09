1184e957afa2528d12906a5985047c87eed7e4db
Jek John Tan <jetan@u90z02s03.42kl.edu.my>
2024-10-09 15:32:57 +0800

    update

--- utils/error_handling.c
+++ utils/error_handling.c
@@ -6,11 +6,21 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/09 15:02:40 by jetan             #+#    #+#             */
-/*   Updated: 2024/10/09 15:16:57 by jetan            ###   ########.fr       */
+/*   Updated: 2024/10/09 15:30:38 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+void	error_message(char *builtin, char *arg, char *mssg)
+{
+	ft_putstr_fd(builtin, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(arg, 2);
+	ft_putstr_fd(": ", 2);
+	ft_putstr_fd(mssg, 2);
+	ft_putstr_fd("\n", 2);
+}
 
 int	arg_count(char **args)
 {
