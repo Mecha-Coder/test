d4369b44bafb0049e9b86f518e012d151f9bc85f
Jek John Tan <jetan@u90z04s02.42kl.edu.my>
2024-10-10 18:33:39 +0800

    add cd_home and cd_previous

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,39 +6,56 @@
 /*   By: jetan <jetan@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/01 14:24:56 by jetan             #+#    #+#             */
-/*   Updated: 2024/10/09 18:53:38 by jetan            ###   ########.fr       */
+/*   Updated: 2024/10/10 18:16:10 by jetan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-
-
-builtin_cd(char **av)
+void	update_pwd()
 {
-	char cwd[1024];
+	
+}
+
+void	cd_previous()
+{
 	char	*oldpwd;
+	
+	oldpwd = getenv("OLDPWD");
+	if (chdir(oldpwd))
+	{
+		printf("%s", oldpwd);
+	}
+}
+
+void	cd_home()
+{
 	char	*home;
 	
-	oldpwd = getenv("PWD");
-	home = getenv("HOME");
+	home = getenv("HOME");	
+	if (!home)
+	{
+		error_msg("cd", "Home not set");
+		return;
+	}
+	chdir(home);
+}
+
+void	builtin_cd(char **av)
+{
+	
 	arg_count(av, "cd");
 	if (!av[1])//no argument
-	{
-		//back to root
-	}
-	else if (ft_strcmp(av[1], '-') == 0)
-	{
-		
-	}
-	else
-	{
-		
-	}
+		cd_home();
+	else if (av[1] == '-')
+		cd_previous();
+	else if ()
 }
 
 int	main(int ac, char **av)
 {
 	(void) ac;
-	builtin_cd(av);
+	// builtin_cd(av);
+	// cd_previous();
+	cd_home();
 }
