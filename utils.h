dbd4ffefad4daeb136b25c88d4b2395fcb85c348
Jek John Tan <jetan@u90z04s02.42kl.edu.my>
2024-10-10 15:24:27 +0800

    upload utils.h

---
+++ utils.h
@@ -0,0 +1,8 @@
+#ifndef UTILS_H
+# define UTILS_H
+
+void	error_arg_msg(char *builtin, char *arg, char *msg);
+void	error_msg(char *builtin, char *msg);
+void	arg_count(char **args, char *builtin);
+
+#endif
